e886490606148cd22d18d8877637fd101171a518
iltafah <eliastafah@gmail.com>
2021-11-03 15:11:56 +0100

    finished working in CPP-01/ex04

--- CPP-01/ex04/main.cpp
+++ CPP-01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iltafah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 18:03:57 by iltafah           #+#    #+#             */
-/*   Updated: 2021/11/02 18:55:58 by iltafah          ###   ########.fr       */
+/*   Updated: 2021/11/03 15:10:47 by iltafah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,61 @@ int		printError(std::string errorMsg)
 	return (ERROR);
 }
 
-int		checkErrors(int argsCount, char **args)
+int		checkErrors(int argsCount, char** args)
 {
 	if (argsCount != 3)
-		return (printError("Error: Wrong number of arguments"));
+		return (printError("Error: Wrong number of arguments!"));
 	if (args[0][0] == '\0' || args[1][0] == '\0' || args[2][0] == '\0')
-		return (printError("Error: An empty string within arguments"));
+		return (printError("Error: An empty string within arguments!"));
 	return (SUCCESS);
 }
 
-int		main(int argc, char **argv)
+int		createOutputFile(std::ofstream &outputFile, std::string OFName)
+{
+	outputFile.open(OFName, std::ofstream::trunc);
+	if (outputFile.fail())
+		return (printError("Error: Faild to create output file!"));
+	return (SUCCESS);
+}
+
+void	replaceOccurrences(std::string &currLine, char* toReplace, char* replaceWith)
+{
+	int			index;
+
+	while ((index = currLine.find(toReplace)) != currLine.npos)
+	{
+		currLine.erase(index, strlen(toReplace));
+		currLine.insert(index, replaceWith);
+	}
+}
+
+void	replaceAndWrite(std::ifstream &inputFile, std::ofstream &outputFile,
+				char *toReplace, char *replaceWith)
+{
+	std::string	currLine;
+
+	while (inputFile.good())
+	{
+		std::getline(inputFile, currLine);
+		replaceOccurrences(currLine, toReplace, replaceWith);
+		outputFile << currLine;
+		if (inputFile.eof() == false)
+			outputFile << std::endl;
+	}
+}
+
+int		main(int argc, char** argv)
 {
 	if (checkErrors(argc - 1, argv + 1) == SUCCESS)
 	{
-		std::ifstream inputFile;
-		
-		inputFile.open(argv[1]);
-		
+		std::ofstream	outputFile;
+		std::ifstream	inputFile(argv[1]);
+
+		if (inputFile.fail())
+			return (printError("Error: Couldn't open the given file!"));
+		if (createOutputFile(outputFile, "FILENAME.replace") == ERROR)
+			return (0);
+		replaceAndWrite(inputFile, outputFile, argv[2], argv[3]);
 	}
 	return (0);
 }
--- CPP-01/ex04/replace.hpp
+++ CPP-01/ex04/replace.hpp
@@ -6,7 +6,7 @@
 /*   By: iltafah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 18:08:07 by iltafah           #+#    #+#             */
-/*   Updated: 2021/11/02 18:54:51 by iltafah          ###   ########.fr       */
+/*   Updated: 2021/11/03 08:40:29 by iltafah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 # include <fstream>
 
 # define SUCCESS 1
-# define ERROR -1
+# define ERROR 0
 
 #endif
